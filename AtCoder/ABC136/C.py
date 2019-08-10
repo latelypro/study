29dedf33ded7bbc345fe96bf8134ba29ca32f268
duchida <d.uchida39@gmail.com>
2019-08-10 19:24:56 +0900

    fix ABC136 C

--- AtCoder/ABC136/C.py
+++ AtCoder/ABC136/C.py
@@ -1,21 +1,12 @@
 n=int(input())
 h=list(map(int,input().split()))
-max=0
-res='Yes'
-for i in range(n):
-    if i == n -1:
-        break
-    diff = h[i] - h[i+1]
-
-    if max < h[i]:
-        max=h[i]
-        if diff == 1:
-            max -= 1
-    max_diff = max - h[i+1]
-    #print(i, diff, max_diff)
-    if  diff > 1 or max_diff > 0:
-        res='No'
-        break
+tmp=h[0]
+for i in range(1,n):
+    if h[i] - tmp == 0:
+        tmp = h[i]
+    elif h[i] - tmp >= 1:
+        tmp = h[i] -1
     else:
-        pass
-print(res)
+        print('No')
+        exit()
+print('Yes')
